1983868eaf17dfd55c104626a25b5cbea877c1ae
Daniel Angulo <dangulo4@gmail.com>
2020-03-13 20:23:57 -0700

    target req.params.id

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,5 +1,5 @@
 //Load data by linking routes
-var path = require('path');
+const fs = require('fs');
 var data = require('../db/db.json');
 
 //Routing
@@ -9,20 +9,35 @@ module.exports = function(app) {
     res.json(data);
   });
 
-  // If no matching route is found default to home
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
-
-  // API POST Request
-  app.post('/api/notes', function(req, res) {
+  // API POST Request -- need to add to file research
+  app.post('/api/notes/', function(req, res) {
     data.push(req.body);
-    res.json(true);
+    res.json({ success: true, msg: 'Created new note' });
   });
 
-  app.post('/api/clear', function(req, res) {
-    data.length = 0;
+  // app.delete('/api/notes/:id', (req, res) => {
+  //   data.length = 0;
 
-    res.join({ ok: true });
+  //   res.json({ ok: true, msg: `Delete bootcamp ${req.params.id}` });
+  // });
+
+  // DELETE "/api/notes" deletes the note with an id equal to req.params.id
+  app.delete('/api/notes/:id', (req, res) => {
+    let noteId = req.params.id;
+    console.log(noteId);
+    fs.readFile('./db/db.json', 'utf8', (err, response) => {
+      if (err) throw err;
+      const allNotes = JSON.parse(response);
+      const newAllNotes = allNotes.filter(note => note.id != noteId);
+      fs.writeFile(
+        './db/db.json',
+        JSON.stringify(newAllNotes, null, 2),
+        err => {
+          if (err) throw err;
+          res.json(data);
+          console.log('Note deleted!');
+        }
+      );
+    });
   });
 };
